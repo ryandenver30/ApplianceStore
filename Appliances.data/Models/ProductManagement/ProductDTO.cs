644cc6dee3e47ccdeff838ae368eefc28a8c4966
ryandenver30 <ryandenver30@gmail.com>
2019-03-25 21:22:32 +0530

    added New API

---
+++ Appliances.data/Models/ProductManagement/ProductDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Appliances.data.Models.ProductManagement
+{
+    public class ProductDTO
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string ModelNo { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public double Weight { get; set; }
+    }
+}
---
+++ Appliances.data/Models/ProductManagement/ProductSubCategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Appliances.data.Models.ProductManagement
+{
+    public class ProductSubCategoryDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string BrandName { get; set; }
+        public string OperatingSystem { get; set; }
+        public string Description { get; set; }
+    }
+}
